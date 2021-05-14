853dea4cef60b17f857bee293620f7046562a377
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-14 12:13:50 -0500

    Added GET route for index.html

--- NoteTaker.js
+++ NoteTaker.js
@@ -6,3 +6,8 @@ const port = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
