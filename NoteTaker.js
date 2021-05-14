d05a5073644afa67b5e00c5541b280960818e53c
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-14 12:49:03 -0500

    Updated NoteTaker.js file

--- NoteTaker.js
+++ NoteTaker.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const indexPage = `${__dirname}/public/index.html`;
+const notesPage = `${__dirname}/public/notes.html`;
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+// Require for POST, both the ".use"
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// Routes - GET - for index.html and notes.html pages
+app.get("/", (req, res) => res.sendFile(indexPage));
+app.get("/notes", (req, res) => res.sendFile(notesPage));
 
+// Initialize-start Express instance
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
