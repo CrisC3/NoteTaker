d9cf58bf4ea80a5fd4a94eb1f970319bb6667e60
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-14 22:38:06 -0500

    Updated NoteTaker.js and gitignore files

--- NoteTaker.js
+++ NoteTaker.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require('fs');
-const { json } = require("express");
 
 const webIndex = `${__dirname}/public/index.html`;
 const webNotes = `${__dirname}/public/notes.html`;
@@ -27,8 +26,76 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
+    
     const newNote = req.body;
+
+    console.log("=================");
+    console.log("request body");
+    console.log(newNote);
+    console.log("newNote length = " + newNote.length);
+    console.log("=================");
+    
+    fs.readFile(dbNotes, "utf8", (errRead, dataRead) => {
+        if (errRead) throw errRead;
+
+        let jsonData = JSON.parse(dataRead);
+
+        jsonData.forEach((element, index, array) => {
+            if (index == array.length - 1) {
+                lastId = element.id;
+                NextId = lastId + 1;
+            }
+        });
+
+        if (newNote.length == undefined) {
+
+            if (newNote.hasOwnProperty("title") && newNote.hasOwnProperty("text"))
+                jsonData.push({id : NextId, title: newNote.title, text : newNote.text});
+            else {
+                let message = apiPostError();
+                res.send(message);
+            }
+        }
+        else {
+            
+            newNote.forEach(element => {
+                
+                console.log("=================");
+                console.log("Individual element");
+                console.log(`title: ${element.title}, text : ${element.text}`);
+                console.log("=================");
+
+                if (element.hasOwnProperty("title") && element.hasOwnProperty("text")) {
+                
+                    jsonData.push({id : NextId++, title: element.title, text : element.text});
+                    console.log(lastId);
+                }
+                else {
+                    let message = apiPostError();
+                    res.send(message);
+                }
+            });
+
+            
+        }
+        
+        console.log("Line 82");
+        console.log(jsonData);
+
+        // fs.writeFile(dbNotes, dataWrite, (errWrite) => {
+        //     if (errWrite) throw errWrite;
+        // });
+
+    });
 });
+
+function apiPostError() {
+    
+    let generalMsgError = "Unable to add data due to incorrect/missing keys { title: '...', text: '...' }";
+    console.log(generalMsgError);
+
+    return generalMsgError;
+}
 
 function getNextId() {
 
