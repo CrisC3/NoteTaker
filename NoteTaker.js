525ac1a85dd1e306c1d0be4bd05126471615949c
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-14 19:22:18 -0500

    Basic functionality test

--- NoteTaker.js
+++ NoteTaker.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const indexPage = `${__dirname}/public/index.html`;
-const notesPage = `${__dirname}/public/notes.html`;
+const path = require("path");
+const fs = require('fs');
+
+const webIndex = `${__dirname}/public/index.html`;
+const webNotes = `${__dirname}/public/notes.html`;
+const dbNotes = `${__dirname}/db/db.json`;
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use("/Static", express.static("public/assets"));
 
 // Require for POST, both the ".use"
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes - GET - for index.html and notes.html pages
-app.get("/", (req, res) => res.sendFile(indexPage));
-app.get("/notes", (req, res) => res.sendFile(notesPage));
+app.get("/", (req, res) => res.sendFile(webIndex));
+app.get("/notes", (req, res) => res.sendFile(webNotes));
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(dbNotes);
+});
 
 // Initialize-start Express instance
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
