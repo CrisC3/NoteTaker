57e6c3647b06b908a2479bffc15fcec435cd704d
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-14 21:30:48 -0500

    Updated code files

--- NoteTaker.js
+++ NoteTaker.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require('fs');
+const { json } = require("express");
 
 const webIndex = `${__dirname}/public/index.html`;
 const webNotes = `${__dirname}/public/notes.html`;
@@ -9,7 +10,9 @@ const dbNotes = `${__dirname}/db/db.json`;
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/Static", express.static("public/assets"));
+let NextId = "";
+
+app.use("/static", express.static("public/assets"));
 
 // Require for POST, both the ".use"
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +25,29 @@ app.get("/notes", (req, res) => res.sendFile(webNotes));
 app.get("/api/notes", (req, res) => {
     res.sendFile(dbNotes);
 });
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+});
+
+function getNextId() {
+
+    let lastId;
+
+    fs.readFile(dbNotes, "utf8", (err, data) => {
+        if (err) throw err;
+
+        let jsonData = JSON.parse(data);
+
+        jsonData.forEach((element, index, array) => {
+            if (index == (array.length - 1))
+                lastId = element.id;
+        });
+
+        NextId = ++lastId;
+
+    });
+}
 
 // Initialize-start Express instance
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
